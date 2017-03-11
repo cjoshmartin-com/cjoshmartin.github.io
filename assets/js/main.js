20bf742363014feef9e6be1d2250650e9e23d4e6
Josh Martin <joshmartin5001@gmail.com>
2017-03-11 01:17:41 -0500

    submit

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,11 @@
 $(document).ready(function(){
-  $('.work-image').mouseenter(function(){
-    
-  }).mouseleave(function(){
+  //on about click
+   //do something cool
+   $(document).on('click',"#about",function () {
+     $(".effect-6").addClass("hidden");
+     $(".work-form").addClass("hidden");
+   });
 
-  });
-
+   //on blog click
+    // hide everything and show what I have written
 });
