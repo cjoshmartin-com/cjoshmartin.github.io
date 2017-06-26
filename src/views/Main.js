53deb0ad49a1f40787f0500a22b83613f895858b
Josh Martin <joshmartin5001@gmail.com>
2017-06-25 22:52:28 -0400

    $1
    $2

--- src/views/Main.js
+++ src/views/Main.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react';
 import * as firebase from 'firebase';
 import {Grid,Row,Col} from 'react-bootstrap';
+import {NavItem,Nav } from 'react-bootstrap';
+
+import { Grid as Grids} from 'semantic-ui-react'
+
 
 class Main extends Component {
   constructor(prop){
@@ -47,18 +51,20 @@ render(){
       <Grid>
         <Row>
           <Col xs={12} sm={12} md={12}>
-      <div>
+      <div className="jsonText">
         <span className="varText">var </span> <span className="varName">introduction</span> = [<span className="interests">{this.state.description}</span>]
       </div>
-      <div>
+      <div className="jsonText">
         <span className="varText">var </span> <span className="varName">job</span> = [<span className="interests">is_hired:</span>{(this.state.job.is_hired =="False") ? <span className="errorText">False</span>:<span className="varName">True</span>} ,<span className="interests">current_job:</span><span className="varName">"{this.state.job.current_job}"</span>]
       </div>
-      <div>
+      <div className="jsonText">
         <span className="varText">var </span> <span className="varName">interest</span> = [{interestsList}]
       </div>
-      <div style={{textAlign:"center"}}>
+      <div style={{textAlign:"center"}} className="jsonText">
         <br />
-        <a href="/about"><span className="varName">Class</span> learnMore(<span className="varName">User newUser</span>)<span style={{color:"black"}}>;</span></a>
+          <Grids centered columns={2}>  {/* this is a hack */}
+        <Nav bsStyle="pills"><NavItem href="about"><span className="varName">Class</span> learnMore(<span className="varName">User newUser</span>)<span style={{color:"black"}}>;</span></NavItem></Nav>
+      </Grids>
       </div>
       </Col>
       </Row>
