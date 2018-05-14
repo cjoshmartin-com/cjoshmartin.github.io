f3ba9706d4dd30088c947618fc2f1d8df98461a1
Josh Martin <joshmartin5001@gmail.com>
2018-05-14 02:30:44 -0400

    Delete Semantic-UI and Fix version of Firebase
    Was having problems with setting up firebase and semantic-ui. Thus, fix firebase's version and delete reference to semantic-ui. Added prop-types.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import registerServiceWorker from './registerServiceWorker';
-import 'semantic-ui-css/semantic.min.css';
+//import 'semantic-ui-css/semantic.min.css';
 import './index.css';
 
 import { HashRouter as Router, Route } from 'react-router-dom'
@@ -11,6 +11,7 @@ import App from './views/App.js'
 ReactDOM.render(
     <Router>
         <Route component={App} />
-    </Router> ,document.getElementById('root')
+    </Router>,
+    document.getElementById('root')
 );
 registerServiceWorker();
--- src/views/App.js
+++ src/views/App.js
@@ -15,7 +15,7 @@ import { Route, Switch } from 'react-router-dom'
 
 import DocumentTitle from 'react-document-title'
 
-class App extends Component {
+export default class App extends Component {
 
     constructor(prop){
         super(prop)
@@ -27,8 +27,8 @@ class App extends Component {
     }
 
     componentWillMount(){
-        db.on('value',snapshot => {
-            this.setState( { data: snapshot.val(), link: snapshot.val().resume.link } )
+        db.on('value',firebaseData => {
+            this.setState( { data: firebaseData.val(), link: firebaseData.val().resume.link } )
         })
     }
 
@@ -37,18 +37,18 @@ class App extends Component {
       <DocumentTitle title="Josh Martin">
       <div className="App" >
         <Header  name={this.state.data.name} />
+        
         <Switch>
          <Route exact={true} path="/" render={ ()=>( <Main {...this.state.data} /> ) }/>
          <Route path="/about" render={()=> (<About {...this.state.data}/>)}/>
          <Route path="/projects" render={()=>(<Projects />)}/>
-        <Route exact path="/blog" render={() => (<Blog {... this.state.data.blog} />)} />
+        <Route exact path="/blog" render={() => (<Blog {...this.state.data.blog} />)} />
         <Route path='/blog/:id' render={ (url) => (<BlogPost {...this.state.data.blog} id={url.match.params.id} /> )  } />
         </Switch>
+
         <Footer {...this.state.data} resumelink={this.state.link}/>
       </div>
     </DocumentTitle>
     );
   }
 }
-
-export default App;
