56f65bc5d56e70080628ba0eb645e5c2282d3f9f
Josh Martin <contact@cjoshmartin.com>
2020-04-26 01:11:46 -0400

    Fix(Project Layout): Changes over to css grids

--- src/views/projects/ProjectsUtils.js
+++ src/views/projects/ProjectsUtils.js
@@ -1,26 +1,34 @@
 import React from 'react';
-import { Col} from 'react-bootstrap';
 import {Card, Icon, Image} from 'semantic-ui-react'
 import _ from 'lodash';
 
 function createProjectList(projects = []) {
     return projects.map((list, index) => {
         return (
-            <Col xs={12} sm={8} md={4} key={index} className="projectGrid">
                 <Card key={index} href={list.link} target="_blank" color='grey'>
+
                     <Image src={list.images.small} alt={list.title} fluid />
+
+
                     <Card.Content>
-                        <Card.Header>
-                            {list.title}
-                        </Card.Header>
+
+                        <Card.Header> {list.title} </Card.Header>
+
                         <Card.Description dangerouslySetInnerHTML={{ __html: list.details }} />
+
                     </Card.Content>
+
+
                     <Card.Content extra>
+
                             <Icon name={list.icon.icon} />
+
                             {list.icon.name}
+
                     </Card.Content>
+
+
                 </Card>
-            </Col>
         );
     });
 }
--- src/views/projects/ProjectsView.js
+++ src/views/projects/ProjectsView.js
@@ -1,32 +1,27 @@
 import React from 'react';
-import { Grid, Col, Row } from 'react-bootstrap';
-import {Card} from 'semantic-ui-react'
 import DocumentTitle from 'react-document-title'
 
 import {createProjectList, mergeProjects} from "./ProjectsUtils";
 
+export default function ProjectsView(props){
+        
+        const cards = createProjectList(
+                            mergeProjects(
+                            props.codepen,
+                            props.github,
+                            props.misc_projects
+                            )
+                        )
 
-const ProjectsView = (props) => {
         return (
             <DocumentTitle title={"Josh Martin - Projects"}>
-                <div>
-                    <Row>
-                        <Col md={10} sm={10} lg={10} xsOffset={2} smOffset={2} mdOffset={1} lgOffset={2} >
-                        </Col>
-                    </Row>
-                    <Grid>
-                        <h1>Projects</h1>
-                        <Card.Group itemsPerRow={2} >
-                            {
-                                createProjectList(
-                                    mergeProjects(props.codepen, props.github, props.misc_projects)
-                                )
-                            }
-                        </Card.Group>
-                    </Grid>
+                <div className="projectPage">
+                    <h1 className="projectTitle">Projects</h1>
+                    <div className="projectGrid">
+                        {cards}
+                    </div>
                 </div>
             </DocumentTitle>
         );
     };
 
-export default ProjectsView;
